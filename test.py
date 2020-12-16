c0f0038faf9c63841c2a2c6b082961c657072b43
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-12-17 02:57:53 +0300

    implemented a cbir system

--- test.py
+++ test.py
@@ -6,26 +6,33 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras import models, layers
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import scipy.spatial
 
-# TODO. get a keras model (CNN?) and write it.
-# TODO. give dataset to the model. get some results.
-
-# TODO. get a feature vector from keras model
-# TODO. write a cbir system.
+# TODO. calculate all the prediction values.
 
 
-def get_hsi(ratio=.1, dir='dataset/AnkaraHSIArchive'):
-    X, Y = [], []
+def read_hsi(file):
+    return scipy.io.loadmat(file)['patch']
+
+
+def get_hsi(dir='dataset/AnkaraHSIArchive'):
+    X, Y, names = [], [], []
 
     dfs = pd.read_excel('dataset/AnkaraHSIArchive/Labels.xlsx',
                         sheet_name='Land-Use Categories')
 
     for file in os.listdir(dir):
         if file.endswith('.mat'):
-            X.append(scipy.io.loadmat(os.path.join(dir, file))['patch'])
+            name = os.path.join(dir, file)
+            X.append(read_hsi(name))
             i = int(file.split('_')[0]) - 1
             # print(file, i, dfs.iat[i, 0], dfs.iat[i, 1]) # FIXME. double check.
             Y.append(dfs.iat[i, 1] - 1)
+            names.append(name)
+    return np.array(X), np.array(Y), names
+
+
+def split_hsi(X, Y, ratio=.1):
     return train_test_split(np.array(X), np.array(Y), test_size=ratio, random_state=42)
 
 
@@ -75,13 +82,35 @@ def train_model(model, X_train, Y_train, X_test, Y_test):
     plt.legend(loc='lower right')
 
 
+def k_closest(db, query, k=6):
+    db = [i[0] for i in sorted(enumerate(list(db)), key = lambda p: np.linalg.norm(p[1]-query))]
+    return db[:k]
+
+def cbir(query):
+    # TODO. test on closest
+    db_img, _, db_tag = get_hsi()
+    model = models.load_model('hsi_model')
+    # model.summary()
+    features = models.Model(
+        inputs=model.input, outputs=model.layers[-2].output)
+
+    db_feature = features.predict(db_img)
+    query_feature = db_feature[db_tag.index(query)]
+    closest = k_closest(db_feature, query_feature)
+    print('Retrieved images: ')
+    for i in closest:
+        print(db_tag[i][:-3] + 'bmp')
+
+
+
 # print(dfs)
-X_train, X_test, Y_train, Y_test = get_hsi()
-print(Y_train)
-# show_img(X_train, Y_train)
-model = create_model()
-train_model(model, X_train, Y_train, X_test, Y_test)
-# print(data)
+# X_train, X_test, Y_train, Y_test = get_hsi()
+# print(Y_train)
+# # show_img(X_train, Y_train)
+# model = create_model()
+# train_model(model, X_train, Y_train, X_test, Y_test)
+# # print(data)
+# cbir('dataset/AnkaraHSIArchive/099_EO1H1770322015230110KF_Radiance_25x3.mat')
 
 
 # img = scipy.io.loadmat(
